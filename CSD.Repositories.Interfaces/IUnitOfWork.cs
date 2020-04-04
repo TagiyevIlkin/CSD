d00e96b943cabfbde942de798a21e65c6d8ccc4c
Ilkin <ilkintagiyev06@gmail.com>
2020-04-04 04:20:26 -0700

    Repository_Created

---
+++ CSD.Repositories.Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using CSD.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSD.Repositories.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<T> Repository<T>() where T : class;
+
+        Task<TransResult<int>> Commit();
+
+        void Rollback();
+    }
+}
